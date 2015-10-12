0e1d67e6374457d8e41617690b894a4c319dcbf3
S0lll0s <s0lll0s@blinkenshell.org>
2015-10-12 18:48:01 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,50 @@
+Magnetman
+=========
+
+*Magnetman* is a small python tool that listens for keystrokes and lid-switch patterns, activated by tapping the lid-switch once.
+It can be used to add little extra commands to trigger using magnetic implants.
+
+Usage
+-----
+All rules must start with a tap in order to be recognized by *Magnetman*.
+Other than taps, which are lid events shorter than .5 seconds, there are "lid holds" and keystrokes.
+A sequence starts with a tap and consist of any number of other such events.
+A rule maps a sequence to a system command to be executed when the sequence is matched.
+
+Elevated privileges may be required to access the lidswitch or grab the keyboard focus.
+
+### "standalone"
+*Magnetman* comes with a convenient CLI for defining rules on the commandline.
+
+    usage: magnetman.py [-h] [-l LID] [-k KBRD] [rule [rule ...]]
+
+    positional arguments:
+      rule                  a rule to follow
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -l LID, --lid LID     evdev name of the Lid Switch
+      -k KBRD, --kbrd KBRD  evdev name of the keyboard
+
+A rule consists of the sequence to match, followed by a colon (`:`) and the command string to execute.
+Sequences are strings of lowercase characters, didigts and periods, representing keypresses, lid holds (the digit is the hold length in seconds) and taps.
+
+Example:
+
+    sudo python magnetman.py '...:echo three taps' '.2a:echo tap, two second hold and an A'
+
+### as a module
+More sophisticated rules can be created via the method `add_rule`:
+
+```python
+from magnetman import MagnetMan
+magnetman = MagnetMan("keyboard", "Lid")
+magnetman.add_rule(['tap', ('key', 'a'), ('lid', 1), ('lid', 1, 3), 'tap'], "echo test")
+magnetman.watch()
+```
+
+Like on the CLI, there are three possible sequence entries:
+
+* taps, which are .5s long, represented as the string `'tap'`,
+* key presses, represented as a tuple of the string `'tap'` and the key,
+* lid holds, represented as the string `'lid'` and the upper and lower length-boundaries in seconds. If the upper limit is left out, a margin of .5s is used around that length.
---
+++ magnetman.py
@@ -0,0 +1,135 @@
+import os
+import asyncio
+from evdev import InputDevice, ecodes, categorize, list_devices
+
+class MagnetMan(object):
+    TAP_LENGTH      = .5
+    SUBMIT_DELAY    = .7
+    LID_TOLERANCE   = .5
+
+    def __init__(self, kbname, lidname):
+        self.key = next(InputDevice(dev) for dev in list_devices() if kbname in InputDevice(dev).name)
+        self.lid = next(InputDevice(dev) for dev in list_devices() if lidname in InputDevice(dev).name)
+        self.loop = asyncio.get_event_loop()
+        self.listening = False
+        self.sequence = []
+        self.finish_handle = None
+        self.lastlid = 0
+
+        self.patterns = []
+
+    def add_command(self, pattern, action):
+        self.patterns.append((pattern, action))
+
+    def watch(self):
+        self.loop.add_reader(self.lid, self.handle_lid_evts)
+        self.loop.add_reader(self.key, self.handle_key_evts)
+        self.loop.run_forever()
+
+    def process(self, sequence):
+        for (pat, action) in self.patterns:
+            if len(pat) != len(sequence):
+                continue
+
+            for exp, act in zip(pat, sequence):
+                if exp == act:
+                    continue
+
+                if exp[0] != act[0]:
+                    break
+
+                if exp[0] == 'key':
+                    if exp[1] != act[1]:
+                        break
+                elif exp[0] == 'lid':
+                    if len(exp) > 2: # min & max
+                        if act[1] > exp[1] or act[1] > exp[2]:
+                            break
+                    elif act[1] < exp[1] - self.LID_TOLERANCE/2 or act[1] > exp[1] + self.LID_TOLERANCE/2:
+                        break
+            else:
+                print("executing '{}'...".format(action))
+                os.system(action)
+                break
+        else:
+            print("no command found for {}".format(sequence))
+
+    def finish(self):
+        self.process(self.sequence)
+        self.sequence.clear()
+
+        if self.listening:
+            try:
+                self.key.ungrab()
+                self.listening = False
+            except IOError: pass
+
+    def reschedule(self):
+        if self.finish_handle:
+            self.finish_handle.cancel()
+        self.finish_handle = self.loop.call_later(self.SUBMIT_DELAY, self.finish)
+
+    def handle_lid_evts(self):
+        for evt in self.lid.read():
+            if evt.type == ecodes.EV_SW and evt.code == ecodes.SW_LID:
+                if evt.value == 0: # lid released
+                    if self.lastlid + self.TAP_LENGTH > evt.timestamp(): # short tap
+                        self.sequence.append('tap')
+                        self.lastlid = evt.timestamp()
+                        self.reschedule()
+                        if not self.listening:
+                            try:
+                                self.key.grab()
+                                self.listening = True
+                            except IOError: pass
+                    elif self.listening:
+                        self.sequence.append(('lid', evt.timestamp()-self.lastlid))
+                        self.reschedule()
+                else: # lid down
+                    self.lastlid = evt.timestamp()
+                    if self.finish_handle:
+                        self.finish_handle.cancel()
+
+    def handle_key_evts(self):
+        for evt in self.key.read():
+            if evt.type == ecodes.EV_KEY and self.listening:
+                if evt.value == 0: # key up
+                    self.sequence.append(('key', ecodes.KEY[evt.code][4:].lower()))
+                    self.reschedule()
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-l', '--lid', default="Lid",
+                        help="evdev name of the Lid Switch")
+    parser.add_argument('-k', '--kbrd', default="keyboard",
+                        help="evdev name of the keyboard")
+    parser.add_argument('rule', nargs='*', default=['.1.1:killall i3lock', '..a:echo test'],
+                        help="""Rule definition syntax:
+                        <sequence>:<command>
+
+                        where sequence starts with a tap and consists of the following:
+                        .: a tap;
+                        N: (a number) that many seconds of holding;
+                        a: (a character) that key.
+
+                        and commands is an arbitrary shell command to be executed.
+                        """)
+    args = parser.parse_args()
+
+    magnetman = MagnetMan(args.kbrd, args.lid)
+
+    for rule in args.rule:
+        rule, action = rule.split(":", 1)
+        seq = []
+        for char in rule:
+            if char == ".":
+                seq.append('tap')
+            else:
+                try:
+                    seq.append(('lid', int(char)))
+                except ValueError:
+                    seq.append(('key', char))
+        magnetman.add_command(seq, action)
+
+    magnetman.watch()
